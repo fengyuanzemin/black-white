aefa606a7ede8fa62a361d6267d05b6c41bbcf37
fengyuanzemin <fengyuanzemin@gmail.com>
2016-09-17 18:34:36 +0800

    滚动动画
    异步处理没写好

--- src/white.js
+++ src/white.js
@@ -3,19 +3,39 @@
  */
 
 
-const container = document.querySelector('.container');
+const CONFIG = {
+    container: document.querySelector('.container')
+};
 
 
-window.onload = ()=> {
+window.onload = () => {
     init();
 };
 
+
+function init() {
+    generateBlackWhite();
+    getRule();
+    roll();
+    setInterval(roll, 4000);
+
+}
+
+
 function generateBlackWhite() {
-    // 给row-item下面的随机一个col-item赋值
+    // 页面第一次加载的时候
+    const classIndex = ['first', 'second', 'third'];
     const item = [...document.querySelectorAll('.row-item .col-item')];
-    for(let i =0 ;i<item.length;i+=1){
+    // 按顺序排序
+    [...document.querySelectorAll('.row-item')].map((v, i)=> {
+        v.classList.add(classIndex[i]);
+        return item;
+    });
+    // 先将所有的变成白块
+    for (let i = 0; i < item.length; i += 1) {
         item[i].classList.add('white');
     }
+    // 然后随机变黑块
     for (let i = 0; i < item.length; i += 3) {
         const random = Math.floor(Math.random() * 3);
         item[random + i].classList.remove('white');
@@ -23,7 +43,79 @@ function generateBlackWhite() {
     }
 }
 
+function changeBlackWhite() {
+    const item = [...document.querySelectorAll('.row-item')];
+    item.map((v) => {
+        if (v.className === 'row-item insert roll') {
+            v.className = 'row-item first';
+        } else if (v.className === 'row-item first roll') {
+            v.className = 'row-item second';
+        } else if (v.className === 'row-item second roll') {
+            v.className = 'row-item third';
+        }
+    });
+}
 
-function init() {
-    generateBlackWhite();
+
+function getRule() {
+    CONFIG.container.addEventListener('click', (e) => {
+        const target = e.target;
+        switch (target.className) {
+            case 'col-item white':
+                console.log('game over');
+                break;
+            case 'col-item black':
+                target.classList.remove('black');
+                target.classList.add('white');
+                break;
+            default:
+                break;
+        }
+    });
+
+}
+
+
+function roll() {
+    const firstItem = document.querySelector('.row-item');
+    const newItem = createRowItem();
+    const promise = new Promise((resolve) => {
+        // 添加节点
+        CONFIG.container.insertBefore(newItem, firstItem);
+        resolve();
+    });
+    promise.then(()=> {
+        // 开始滚动
+        [...document.querySelectorAll('.row-item')].map((v)=> {
+            if (!v.classList.contains('roll')) {
+                v.classList.add('roll');
+            }
+            return this;
+        });
+        // 异步删除节点,并重新生成class
+        setTimeout(()=> {
+            removeRowItem();
+            changeBlackWhite();
+        }, 3000);
+    });
+}
+
+function createRowItem() {
+    let colItem;
+    const random = Math.floor(Math.random() * 3);
+    const rowItem = document.createElement('div');
+    rowItem.setAttribute('class', 'row-item insert');
+    for (let i = 0; i < 3; i += 1) {
+        colItem = document.createElement('div');
+        i === random ? colItem.setAttribute('class', 'col-item black')
+            : colItem.setAttribute('class', 'col-item white');
+        rowItem.appendChild(colItem);
+    }
+    return rowItem;
+}
+
+
+function removeRowItem() {
+    const lastChild = CONFIG.container.lastElementChild;
+    CONFIG.container.removeChild(lastChild);
 }
